678424d5ade8bd9d6bc26540e50ebd1c327fe50c
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-09-09 19:36:36 -0300

    :arrow_up: Estrutura de rotas para duas paginas

--- lista-contatos/src/app/app-routing.module.ts
+++ lista-contatos/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { ListContactsComponent } from './list-contacts/list-contacts.component';
+import { NewContactComponent } from './new-contact/new-contact.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'contatos', pathMatch: 'full'},
+  {path: 'cadastro', component: NewContactComponent },
+  {path: 'contatos', component: ListContactsComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
