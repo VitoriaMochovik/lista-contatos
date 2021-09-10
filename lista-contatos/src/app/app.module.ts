43ebeadb1f944e22b5371b1d6ffed075b3427590
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-09-10 10:42:53 -0300

    :arrow_up: Atualização de lógica de verificação de edição ou criação de contatos e estilização da primeira pagina

--- lista-contatos/src/app/app.module.ts
+++ lista-contatos/src/app/app.module.ts
@@ -1,7 +1,12 @@
+import { MatFormField } from '@angular/material/form-field';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from "@angular/common/http"
+import { MatInputModule} from "@angular/material/input"
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {  MatButtonModule} from "@angular/material/button"
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +14,7 @@ import { NewContactComponent } from './new-contact/new-contact.component';
 import { ListContactsComponent } from './list-contacts/list-contacts.component';
 import { EventEmitter } from '@angular/core';
 import { UpdateContactComponent } from './update-contact/update-contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -23,6 +29,12 @@ import { UpdateContactComponent } from './update-contact/update-contact.componen
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
